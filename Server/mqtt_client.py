5c961bc713b7fa4ea1ac873e4bb1e023e357e120
Dmytro Poslavskyi <dmytro.poslavskyi@student.tuke.sk>
2024-01-31 13:23:43 +0100

    fix problems

--- Server/mqtt_client.py
+++ Server/mqtt_client.py
@@ -6,12 +6,14 @@ message_queue = queue.Queue()
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
-    client.subscribe("gateway/zigbee/socket")
-    client.subscribe("gateway/zigbee/window")
-    client.subscribe("gateway/zigbee/temperature_humidity")
+    #client.subscribe("gateway/zigbee/socket")
+    client.subscribe("gateway/zigbee/door sensor")
+    #client.subscribe("gateway/zigbee/temperature_humidity")
 
 def on_message(client, userdata, msg):
+    
     message = {'topic': msg.topic, 'payload': msg.payload.decode()}
+    print("Received message:", json.dumps(message))
     message_queue.put(json.dumps(message))
 
 def create_mqtt_client():
@@ -19,5 +21,5 @@ def create_mqtt_client():
     mqtt_client.username_pw_set('maker', 'this.is.mqtt')
     mqtt_client.on_connect = on_connect
     mqtt_client.on_message = on_message
-    mqtt_client.connect("10.0.0.1", 8080, 60)
+    mqtt_client.connect("147.232.34.94", 1883, 60)
     return mqtt_client
--- Server/sse_controller.py
+++ Server/sse_controller.py
@@ -1,18 +1,21 @@
 from flask import Response, stream_with_context
 from mqtt_client import message_queue
 import time
+from flask_cors import CORS
 
 def sse_stream():
     def event_stream():
         while True:
             if not message_queue.empty():
                 message = message_queue.get()
+                print(f"data: {message}\n\n")
                 yield f"data: {message}\n\n"
             time.sleep(1)
 
     return stream_with_context(event_stream())
 
 def register_sse_routes(app):
+    CORS(app)
     @app.route('/events')
     def sse_request():
         return Response(sse_stream(), content_type='text/event-stream')
